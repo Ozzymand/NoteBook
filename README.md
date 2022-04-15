91c8c9e61bd8c9b3c51940f4d035aea8c1ebc678
Ozzymand <24418935+Ozzymand@users.noreply.github.com>
2022-04-15 23:06:25 +0300

    Bump changes, is this legal?

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
-# Unnamed NoteBook
+# NightBook
 
 [![Release](https://img.shields.io/github/v/release/Ozzymand/NoteBook?include_prereleases)]((https://github.com/Ozzymand/NoteBook/releases/latest))
 [![Release date](https://img.shields.io/github/release-date/Ozzymand/NoteBook)](https://github.com/Ozzymand/NoteBook/releases/latest)
 ![Downloads](https://img.shields.io/github/downloads/Ozzymand/NoteBook/total)
 [![License](https://img.shields.io/github/license/Ozzymand/NoteBook)](https://mit-license.org/)
 
-![Logo banner](https://raw.githubusercontent.com/Ozzymand/NoteBook/main/Images/Logo.png)
+![App logo](images/NightBook_Icon_64.png) 
+![Banner](images/Banner.png)
 
 A OneNote clone to surpass the origin.
 
@@ -31,7 +32,7 @@ A OneNote clone to surpass the origin.
 
 ## How to build
 
-Open the project file inside `Notebook/src/` and build the release using visual studio or your compiler of choice.
+Open the project file inside `/Notebook/` and build the release using visual studio or your compiler of choice.
 
 ---
 
--- src/NoteBook.cpp
+++ src/NoteBook.cpp
@@ -1,39 +1,53 @@
 // NoteBook.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+// system headers
 #include <iostream>
 #include <fstream>
+
 #include <SDL.h>
 
-#include "dependencies/headers/file_handler.h" // Functions: readFile(filePath)
+// own headers
+#include <file_handler.h> // Functions: readFile(filePath)
 
 using namespace std;
 
-/**
- * Main Function
- *
+/** Main Function
+ ****************
  * Takes the following arguemnts
  * argv[0]: main.exe
  * argv[1]: file path
- * 
- **/
+ ****************/
 int main(int argc, char* argv[])
 {
 
 	readFile("test.txt");
-	
-	/*
+
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_Window *window = SDL_CreateWindow("Title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
-	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+	bool isRunning = true;
+	SDL_Event event;
+	
+	while (isRunning){
+		while (SDL_PollEvent(&event)){
+			switch (event.type){
+				case SDL_QUIT:
+					isRunning = false;
+					break;
 
-	SDL_RenderClear(renderer); // Clear the display of previous frame
+				case SDL_KEYDOWN:
+					if (event.key.keysym.sym == SDLK_ESCAPE){
+						isRunning = false;
+					}
+					break;
+			}
+		}
+		SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+		SDL_RenderClear(renderer); // Clear the display of previous frame
+		SDL_RenderPresent(renderer); // Present whatever we manipulated renderer to be
+	}
 
-	SDL_RenderPresent(renderer); // Present whatever we manipulated renderer to be
 
-	SDL_Delay(3000); // Wait for 3 seconds, then bye bye
-	*/
-    
 	return 0;
 }
